4308754d9f20cd257d785cf067baea1d64f6aa4e
Vr3n <vickyspatel@gmail.com>
2023-04-06 20:26:33 +0530

    Login and Logout Logic implemented.
    Integrated HTMX and Sweetalert for notifications.
    Login is handled by native DJango, and Logout is
    handled by Htmx.
    
    close: #8

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponseRedirect, reverse
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .forms import LoginForm, SignUpForm
 
 # Create your views here.
+
+
+def login_view(request):
+    form = LoginForm()
+
+    msg = None
+
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            remember = form.cleaned_data.get("remember_me")
+            user = authenticate(email=email, password=password)
+            if user is not None:
+                login(request, user)
+                if remember:
+                    request.session.set_expiry(1209600)
+                messages.success(request, "Logged in successfully!")
+                return redirect("/")
+            messages.error(request, "There are errors in credentials!")
+        else:
+            messages.error(request,"Error validating the form")
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "accounts/login.html", context)
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, "You have logged out successfully!")
+    response = HttpResponseRedirect(reverse('login'))
+    response["HX-Redirect"] = reverse("login")
+    response["HX-Refresh"] = True
+    return response
+
+def register_view(request):
+    return render(request, "accounts/login.html", context)
--- crm/urls.py
+++ crm/urls.py
@@ -22,5 +22,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("authentication.urls")),
     path("", include("home.urls")),
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
